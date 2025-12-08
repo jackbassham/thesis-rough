2cb76e7cc3402f2c115ddf31f5810876b585414f
jackbassham <jbassham@ucsd.edu>
2025-12-07 16:28:38 -0800

    added dynamic global variables via bash weighted lr

--- 6-lr-weighted/lr_weighted_cf.py
+++ 6-lr-weighted/lr_weighted_cf.py
@@ -3,10 +3,59 @@ from numpy import linalg as LA
 import os
 from scipy.sparse import diags
 
-PATH_SOURCE = "/home/jbassham/jack/data/w92_20/inputs"
-PATH_DEST = "/home/jbassham/jack/data/w92_20/outputs"
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Get global variables from master 'run-data-processing.sh'
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-EPSILON = 0.001
+HEM = os.getenv("HEM") # Hemisphere (sh or nh)
+
+START_YEAR = int(os.getenv("START_YEAR")) # data starts 01JAN<START_YEAR>
+END_YEAR = int(os.getenv("END_YEAR")) # data ends 31DEC<END_YEAR>
+
+TIMESTAMP_IN = os.getenv("TIMESTAMP_IN") # timestamp version of input data
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Paths to data directories defined here
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Get current script directory path
+script_dir = os.path.dirname(__file__)
+
+# Define masked & normalized input data path; relative to current
+PATH_SOURCE = os.path.abspath(
+    os.path.join(
+        script_dir, 
+        '..', 
+        'data', 
+        'mask-norm', 
+        HEM,
+        TIMESTAMP_IN)
+)
+
+# Create the directory if it doesn't already exist
+os.makedirs(PATH_SOURCE, exist_ok=True)
+
+# Define model output data input path; relative to current
+PATH_DEST = os.path.abspath(
+    os.path.join(
+        script_dir, 
+        '..', 
+        'data', 
+        'model-output',
+        'lr-weighted', 
+        HEM,
+        TIMESTAMP_IN)
+)
+
+# Create the directory if it doesn't already exist
+os.makedirs(PATH_DEST, exist_ok=True)
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Additonal variables defined here
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+EPSILON = 0.001 # correction to avoid division by zero in weighting
+
 
 def lr_weighted_gridwise(invars, epsilon):
     
