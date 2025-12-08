e1bd000c3e097cf26241d3ec8713d1d12c005026
jackbassham <jbassham@ucsd.edu>
2025-12-07 16:26:08 -0800

    added dynamic global variables via bash for lr

--- 5-lr/lr_cf.py
+++ 5-lr/lr_cf.py
@@ -2,8 +2,52 @@ import numpy as np
 from numpy import linalg as LA
 import os
 
-PATH_SOURCE = "/home/jbassham/jack/data/ece228/inputs"
-PATH_DEST = "/home/jbassham/jack/data/ece228/outputs"
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Get global variables from master 'run-data-processing.sh'
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+HEM = os.getenv("HEM") # Hemisphere (sh or nh)
+
+START_YEAR = int(os.getenv("START_YEAR")) # data starts 01JAN<START_YEAR>
+END_YEAR = int(os.getenv("END_YEAR")) # data ends 31DEC<END_YEAR>
+
+TIMESTAMP_IN = os.getenv("TIMESTAMP_IN") # timestamp version of input data
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Paths to data directories defined here
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Get current script directory path
+script_dir = os.path.dirname(__file__)
+
+# Define masked & normalized input data path; relative to current
+PATH_SOURCE = os.path.abspath(
+    os.path.join(
+        script_dir, 
+        '..', 
+        'data', 
+        'mask-norm', 
+        HEM,
+        TIMESTAMP_IN)
+)
+
+# Create the directory if it doesn't already exist
+os.makedirs(PATH_SOURCE, exist_ok=True)
+
+# Define model output data input path; relative to current
+PATH_DEST = os.path.abspath(
+    os.path.join(
+        script_dir, 
+        '..', 
+        'data', 
+        'model-output',
+        'lr', 
+        HEM,
+        TIMESTAMP_IN)
+)
+
+# Create the directory if it doesn't already exist
+os.makedirs(PATH_DEST, exist_ok=True)
 
 
 def lr_gridwise(invars):
