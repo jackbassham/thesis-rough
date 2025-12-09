962df1166c4f3282bfaec1a77cdcf8a6af29e4be
jackbassham <jbassham@ucsd.edu>
2025-12-09 15:07:41 -0800

    changed code style for file save

--- 2-regrid/regrid_concentration_nimbus7.py
+++ 2-regrid/regrid_concentration_nimbus7.py
@@ -141,7 +141,8 @@ def main():
         time = time, 
         lat = lat_new, 
         lon = lon_new, 
-        var_names = var_names)
+        var_names = var_names
+        )
 
     print(f"Variables Saved at path {PATH_DEST}/{fnam}")
 
