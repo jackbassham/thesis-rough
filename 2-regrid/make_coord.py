ea96da25e4ff69837b6f2979d347148d3075eb33
jackbassham <jbassham@ucsd.edu>
2025-12-11 13:21:34 -0800

    add function to assert coordinate variables consistent

--- 2-regrid/make_coord.py
+++ 2-regrid/make_coord.py
@@ -57,32 +57,78 @@ def main():
 
     # Load in coordinate variables from motion dataset
     data = np.load(os.path.join(PATH_SOURCE, f'motion_ppv4_latlon_{FSTR_END_IN}.npz'))
-    time = data['time']
-    lat = data['lat']
-    lon = data['lon']
+    time_icevel = data['time']
+    lat_icevel = data['lat']
+    lon_icevel = data['lon']
 
     # Load in coordinate variables from wind dataset
     data = np.load(os.path.join(PATH_SOURCE, f'wind_jra55_latlon_{FSTR_END_IN}.npz'))
-    time = data['time']
-    lat = data['lat']
-    lon = data['lon']
+    time_wind = data['time']
+    lat_wind = data['lat']
+    lon_wind = data['lon']
 
     # Load in coordinate variables from concentration dataset
     data = np.load(os.path.join(PATH_SOURCE, f'con_nimbus7_latlon_{FSTR_END_IN}.npz'))
-    time = data['time']
-    lat = data['lat']
-    lon = data['lon']
+    time_con = data['time']
+    lat_con = data['lat']
+    lon_con = data['lon']
 
+    # Assert latitude coordinate variables are consistent accross datasets
+    assert_equal(
+        {
+        "lat_icevel": lat_icevel,
+        "lat_wind": lat_wind,
+        "lat_con": lat_con
+        }
+    )
 
-    # Save coordinate variables in new file
+    # Assert longitude coordinate variables are consistent accross datasets
+    assert_equal(
+        {
+        "lon_icevel": lon_icevel,
+        "lon_wind": lon_wind,
+        "lon_con": lon_con
+        }
+    )
+
+    # Assert time coordinate variables are consistent accross datasets
+    assert_equal(
+        {
+        "time_icevel": time_icevel,
+        "time_wind": time_wind,
+        "time_con": time_con
+        }
+    )
+
+    # Save coordinate variables from one dataset in new file
     np.savez(
         os.path.join(PATH_DEST, f'coord_{FSTR_END_COORD}'),
-        time = time,
-        lat = lat,
-        lon = lon
+        time = time_icevel,
+        lat = lat_icevel,
+        lon = lon_icevel
     )
 
     return
+
+def assert_equal(coord_dict):
+    """
+    Asserts arrays are equal
+    
+    Takes input 'coord_dict' mapping name to array
+        Example: {"a": a, "b": b, "c": c}
+
+    If arrays are equal, program continues.
+    If not, program strops and value error is raised with print statement.
+    """
+
+    # Pull variable names from dict keys
+    names = list(coord_dict.keys())
+    for i in range (len(names)):
+        for j in range(i + 1, len(names)):
+            a, b = names[i] ,names[j]
+            if not np.array_equal(coord_dict[a], coord_dict[b]):
+                raise ValueError(f"ERROR: {a} and {b} not equal")
+            
 
 if __name__ == "__main__":
     main()
