64446b746673b6b9d77f3ac3ecf8534d2cfd59e7
jackbassham <jbassham@ucsd.edu>
2025-12-11 13:31:47 -0800

    add eval functions, start plot function, add lat lon global var for plots

--- 9-evaluate/eval.py
+++ 9-evaluate/eval.py
@@ -18,6 +18,10 @@ TIMESTAMP_IN = os.getenv("TIMESTAMP_IN") # timestamp version of input data
 
 TIMESTAMP_MODEL = os.getenv("TIMESTAMP_MODEL") # timestamp version of model run
 
+# NOTE: Need lat and lon limits for cartopy plots; should match the input data
+LAT_LIMITS = [float(x) for x in os.getenv("LAT_LIMITS").split(",")] # South to North latitude bounds, degrees
+LON_LIMITS = [float(x) for x in os.getenv("LON_LIMITS").split(",")] # West to East longitude bounds, degrees
+
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Paths to data directories defined here
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -40,7 +44,101 @@ PATH_SOURCE = os.path.abspath(
 
 def main():
 
+    # Get filename from 
+
     # Load in true and predicted model outputs
-    data = np.load(os.path.join(PATH_SOURCE, )) 
+    data = np.load(os.path.join(PATH_SOURCE, fnam)) 
+
+    upred = data['y_pred'][:,0,:,:]
+    vpred = data['y_pred'][:,1,:,:]
+
+    utrue = data['y_true'][:,0,:,:]
+    vtrue = data['y_true'][:,1,:,:]
 
     return
+
+
+def correlation(pred, true):
+
+    """
+    Pearson Correlation
+    """
+
+    predbar = np.nanmean(pred, axis = 0) # mean predicted
+    truebar = np.nanmean(true, axis = 0) # mean true
+
+    covariance = np.nansum((pred - predbar) * (true - truebar), axis = 0) # covariance between predicted and true
+    
+    stdpred = np.sqrt(np.nansum((pred - predbar)**2, axis = 0)) # standard deviation predited
+    stdtrue = np.sqrt(np.nansum((true - truebar)**2, axis = 0)) # standard deviation true
+
+    correlation = covariance / (stdpred * stdtrue)
+
+    return correlation
+
+
+def weighted_correlation(pred, true, r, epsilon = 1e-4):
+
+    """
+    Weighted Pearson Correlation referenced from:
+    https://www.air.org/sites/default/files/2021-06/Weighted-and-Unweighted-Correlation-Methods-Large-Scale-Educational-Assessment-April-2018.pdf
+    
+    """
+
+    w = 1 / (r + epsilon)
+
+    def weighted_mean(x, w):
+        return np.nansum(w * x, axis = 0) / np.nansum(w, axis = 0)
+
+    predbar = weighted_mean(pred, w) # weighted mean predicted
+    truebar = weighted_mean(true, w) # weighted mean true
+
+    weighted_cov = np.nansum(w * (pred - predbar) * (true - truebar), axis = 0) # weighted covariance between predicted and true
+    
+    weighted_stdpred = np.sqrt(np.nansum(w * (pred - predbar)**2, axis = 0)) # weighted standard deviation predited
+    weighted_stdtrue = np.sqrt(np.nansum(w * (true - truebar)**2, axis = 0)) # weighted standard deviation true
+
+    correlation = weighted_cov / (weighted_stdpred * weighted_stdtrue)
+
+    return correlation
+
+
+def skill(pred, true, epsilon = 1e-4):
+    # NOTE excluding epsilon = 1e-4 from denominator for now
+
+    mse = np.nanmean((true - pred)**2, axis = 0) # mean square error
+    # NOTE above is not equivalent to np.nanvar(true-pred), which excludes bias term
+    # MSE = E[(y-x)^2]
+    # = (E[y-x])^2 + Var(y-x)
+    # = bias^2 + Var(y-x)
+    # Can prove the above
+
+    truebar = np.nanmean(true, axis = 0) # mean true
+
+    vartrue = np.nanmean((true - truebar)**2, axis = 0) # variance in true
+    # NOTE above is equivalent to np.nanvar()
+
+    skill = 1 - mse / vartrue
+
+    return skill
+
+def weighted_skill(pred, true, r, epsilon = 1e-4):
+    # NOTE excluding epsilon = 1e-4 from denominator for now
+    # NOTE including epsilon = 1e-4 in the weights in case of uncertainty r ~ 0
+
+    w = 1 / (r + epsilon)
+
+    mse = np.nanmean(( w * (true - pred))**2, axis = 0) # mean square error
+    # NOTE above is not equivalent to np.nanvar(true-pred), which excludes bias term
+
+    truebar = np.nanmean(true, axis = 0) # mean true
+
+    vartrue = np.nanmean(( w * (true - truebar))**2, axis = 0) # variance in true
+    # NOTE above is equivalent to np.nanvar()
+
+    weighted_skill = 1 - mse / vartrue
+
+    return weighted_skill
+
+def plot_metric(u_data, v_data, model_str):
+
