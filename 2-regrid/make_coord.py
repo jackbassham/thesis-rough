1dcad895c09cbbad68059fbe666c5c3c7dc723eb
jackbassham <jbassham@ucsd.edu>
2025-12-10 17:14:19 -0800

    initial commit

---
+++ 2-regrid/make_coord.py
@@ -0,0 +1,68 @@
+import numpy as np
+import os
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Get global variables from master '<  >.sh'
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+HEM = os.getenv("HEM") # Hemisphere (sh or nh)
+
+START_YEAR = int(os.getenv("START_YEAR")) # data starts 01JAN<START_YEAR>
+END_YEAR = int(os.getenv("END_YEAR")) # data ends 31DEC<END_YEAR>
+
+TIMESTAMP_IN = os.getenv("TIMESTAMP_IN") # timestamp version of input data
+
+TIMESTAMP_COORD = os.getenv("TIMESTAMP_COORD") # timestamp version of coordinate data
+
+TIMESTAMP_OUT = os.getenv("TIMESTAMP_OUT") # timestamp version of inputs processed here
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Paths to data directories defined here
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Get current script directory path
+script_dir = os.path.dirname(__file__)
+
+# Define masked & normalized input data path; relative to current
+PATH_SOURCE = os.path.abspath(
+    os.path.join(
+        script_dir, 
+        '..', 
+        'data', 
+        'mask-norm', 
+        HEM,
+        TIMESTAMP_IN)
+)
+
+# Define path to coordinate variables
+PATH_COORD = os.path.abspath(
+    os.path.join(
+        script_dir,
+        '..',
+        'data',
+        'coordinates',
+        HEM,
+        TIMESTAMP_COORD)
+)
+
+# Define model output data input path; relative to current
+PATH_DEST = os.path.abspath(
+    os.path.join(
+        script_dir, 
+        '..', 
+        'data', 
+        'lr-input',
+        HEM,
+        TIMESTAMP_OUT)
+)
+
+# Create the directory if it doesn't already exist
+os.makedirs(PATH_DEST, exist_ok=True)
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Additonal global variables here
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+FSTR_END_IN = f"{HEM}{START_YEAR}{END_YEAR}_{TIMESTAMP_IN}"
+FSTR_END_COORD = f"{HEM}{START_YEAR}{END_YEAR}_{TIMESTAMP_COORD}"
+FSTR_END_OUT = f"{HEM}{START_YEAR}{END_YEAR}_{TIMESTAMP_OUT}"
