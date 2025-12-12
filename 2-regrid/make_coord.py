8734eee9cc10db3ea607b9ad2161ed8c99004ead
jackbassham <jbassham@ucsd.edu>
2025-12-12 13:11:33 -0800

    moved global variables to path.py

--- 2-regrid/make_coord.py
+++ 2-regrid/make_coord.py
@@ -1,57 +1,19 @@
 import numpy as np
 import os
 
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Get global variables from master '<  >.sh'
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-HEM = os.getenv("HEM") # Hemisphere (sh or nh)
-
-START_YEAR = int(os.getenv("START_YEAR")) # data starts 01JAN<START_YEAR>
-END_YEAR = int(os.getenv("END_YEAR")) # data ends 31DEC<END_YEAR>
-
-TIMESTAMP_IN = os.getenv("TIMESTAMP_IN") # timestamp version of input data
-
-TIMESTAMP_COORD = os.getenv("TIMESTAMP_COORD") # timestamp version of coordinate data
-
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Paths to data directories defined here
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-# Get current script directory path
-script_dir = os.path.dirname(__file__)
-
-# Define masked & normalized input data path; relative to current
-PATH_SOURCE = os.path.abspath(
-    os.path.join(
-        script_dir, 
-        '..', 
-        'data', 
-        'regrid', 
-        HEM,
-        TIMESTAMP_IN)
+from .param import (
+    HEM,
+    LAT_LIMITS, 
+    LON_LIMITS,
+    RESOLUTION,
 )
 
-# Define path to coordinate variables
-PATH_DEST = os.path.abspath(
-    os.path.join(
-        script_dir,
-        '..',
-        'data',
-        'coordinates',
-        HEM,
-        TIMESTAMP_COORD)
+from .path import (
+    PATH_SOURCE,
+    PATH_DEST_COORD,
+    FSTR_END_IN,
+    FSTR_END_OUT,
 )
-
-# Create the directory if it doesn't already exist
-os.makedirs(PATH_DEST, exist_ok=True)
-
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Additonal global variables here
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-FSTR_END_IN = f"{HEM}{START_YEAR}{END_YEAR}_{TIMESTAMP_IN}"
-FSTR_END_COORD = f"{HEM}{START_YEAR}{END_YEAR}_{TIMESTAMP_COORD}"
 
 def main():
 
@@ -102,7 +64,7 @@ def main():
 
     # Save coordinate variables from one dataset in new file
     np.savez(
-        os.path.join(PATH_DEST, f'coord_{FSTR_END_COORD}'),
+        os.path.join(PATH_DEST_COORD, f'coord_{FSTR_END_OUT}'),
         time = time_icevel,
         lat = lat_icevel,
         lon = lon_icevel
