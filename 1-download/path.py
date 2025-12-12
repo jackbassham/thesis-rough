d31655279256f41cd4c2a1edd729804c250197fa
jackbassham <jbassham@ucsd.edu>
2025-12-12 11:57:50 -0800

    added string for end of file

--- 1-download/path.py
+++ 1-download/path.py
@@ -1,5 +1,9 @@
 import os
-from .param import HEM, TIMESTAMP_OUT
+from .param import (
+    HEM,
+    START_YEAR, END_YEAR,
+    TIMESTAMP_OUT
+)
 
 # Get current script directory
 script_dir = os.path.dirname(__file__)
@@ -25,3 +29,6 @@ PATH_DEST = os.path.abspath(
 
 # Create the destination directory if it doesn't already exist
 os.makedirs(PATH_DEST, exist_ok = True)
+
+# Define string for end of data file
+FSTR_END_OUT = f"{HEM}{START_YEAR}{END_YEAR}_{TIMESTAMP_OUT}"
