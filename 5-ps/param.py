8c050c7ee4aaca0883ec45de9f3226604ba7fa6b
jackbassham <jbassham@ucsd.edu>
2026-01-04 16:16:23 -0800

    initial commit

---
+++ 5-ps/param.py
@@ -0,0 +1,35 @@
+import os
+import sys
+
+def get_vars(var_name: str) -> str:
+    """
+    Gets global variables from shell script
+
+    Docstring for _get_global_vars
+    
+    :param var_name: Description
+    :type var_name: str
+    :return: Description
+    :rtype: str
+    """
+
+    value = os.getenv(var_name)
+
+    if value is None:
+        print(f"ERROR: Set global variable: {var_name}", file = sys.stderror)
+        sys.exit(1)
+
+    return value
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Required global variables defined here
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+HEM = get_vars("HEM") # hemisphere (sh or nh)
+
+START_YEAR = int(get_vars("START_YEAR")) # data starts 01JAN<START_YEAR>
+END_YEAR = int(get_vars("END_YEAR")) # data ends 31DEC<END_YEAR>
+
+TIMESTAMP_IN = get_vars("TIMESTAMP_IN") # Timestamp version of lr inputs
+
+TIMESTAMP_OUT = get_vars("TIMESTAMP_OUT") # Timestamp version of model output data
---
+++ 5-ps/path.py
@@ -0,0 +1,49 @@
+import os
+from .param import (
+    HEM,
+    START_YEAR, END_YEAR,
+    TIMESTAMP_IN, TIMESTAMP_OUT,
+)
+
+# Get current script directory
+script_dir = os.path.dirname(__file__)
+
+# Get root directory (one level above)
+root = os.path.abspath(
+    os.path.join(
+        script_dir,
+        '..',
+    )
+)
+
+# Define masked-normalized data source path
+PATH_SOURCE = os.path.abspath(
+    os.path.join(
+        root,
+        'data',
+        'lr-input',
+        HEM,
+        TIMESTAMP_IN,
+    )
+)
+
+# Define lr input destination path
+PATH_DEST = os.path.abspath(
+    os.path.join(
+        root,
+        'data',
+        'model-output',
+        'ps',
+        HEM,
+        TIMESTAMP_OUT,
+    )
+)
+
+# Create the destination directory if it doesn't already exist
+os.makedirs(PATH_DEST, exist_ok = True)
+
+# Define string for end of input regrid data file
+FSTR_END_IN = f"{HEM}{START_YEAR}{END_YEAR}_{TIMESTAMP_IN}"
+
+# Define string for end of output data
+FSTR_END_OUT = f"{HEM}{START_YEAR}{END_YEAR}_{TIMESTAMP_OUT}"
