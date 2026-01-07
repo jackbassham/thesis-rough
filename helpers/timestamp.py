b960ef979da37a203825ca111a6a188d7b294eb7
jackbassham <jbassham@ucsd.edu>
2026-01-07 11:32:03 -0800

    change format %Y%m%d_%H%M%S

--- helpers/timestamp.py
+++ helpers/timestamp.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 # Generate time stamp with format #ddMMMYY_HHMM
-timestamp = datetime.now().strftime("%d%b%Y_%H%M")
+timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
 
 # Print timestamp for bash script
 print(timestamp)
