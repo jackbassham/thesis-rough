f9fffddf0bab3f75ca61eb5f13aae83cb9305d4c
jackbassham <jbassham@ucsd.edu>
2026-03-05 14:29:28 -0800

    added command line arguments for pipeline start and stop points to parser

--- _00_config/parse_args.py
+++ _00_config/parse_args.py
@@ -6,7 +6,7 @@ def parse_args():
     # Assign instance of argument parser
     parser = argparse.ArgumentParser()
 
-    # Define data step timestamp version arguments for parser
+    # Define data stage timestamp command line arguments 
     parser.add_argument('--timestamp_out')
     parser.add_argument('--timestamp_raw')
     parser.add_argument('--timestamp_regrid')
@@ -14,6 +14,22 @@ def parse_args():
     parser.add_argument('--timestamp_mask_norm')
     parser.add_argument('--timestamp_model_inputs')
     parser.add_argument('--timestamp_model_output')
+
+    # Define command line argument for pipeline start point (optional partial run)
+    parser.add_argument(
+        '--start',
+        type = str,
+        default = None,
+        help = 'Pipeline step to start from, runs from start point on'
+    )
+
+    # Define command line argument for pipeline stop point (optional partial run)
+    parser.add_argument(
+        '--stop',
+        type = str,
+        default = 'None',
+        help = 'Pipeline step to end on, runs up until stop point'
+    )
 
     # Return the arguments
     return parser.parse_args()
