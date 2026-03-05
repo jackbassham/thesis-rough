5ce19e1573d9d38e8f29a36f28757c279e0dead2
jackbassham <jbassham@ucsd.edu>
2026-03-05 15:06:38 -0800

    initial commit for master run pipeline script

---
+++ run_pipeline.py
@@ -0,0 +1,26 @@
+from _00_config.load_config import load_config
+
+
+from _00_config.parse_args import parse_args
+
+# Define pipeline steps
+PIPELINE_STEPS = [
+    
+]
+
+
+def main():
+
+    # Instantiate configuration
+    config = load_config()
+
+
+def run_pipeline(config, start = None, end = None):
+    """
+    
+    """
+
+
+if __name__ == '__main__':
+    main()
+
