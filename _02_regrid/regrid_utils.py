3b90720bb0b59e44450ce38a8b123e7ac4bd6fee
jackbassham <jbassham@ucsd.edu>
2026-03-10 12:50:20 -0700

    initial commit of abstracting regrid functions into utils module

---
+++ _02_regrid/regrid_utils.py
@@ -0,0 +1,13 @@
+
+
+def main():
+
+    ...
+
+
+def nearest_neighbor_interpolation(
+    grid_resolution, latitude_bounds, longitude_bounds, old_latitude, old_longitude
+    ):
+    """
+    
+    """
