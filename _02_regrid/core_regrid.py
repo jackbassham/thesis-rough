f9985592d34d8d56e03516b5b4b327e96ea0d823
jackbassham <jbassham@ucsd.edu>
2026-03-11 14:59:33 -0700

    implement vector rotation and regrid functions with error handling, added FIXMEs and NOTEs

--- _02_regrid/core_regrid.py
+++ _02_regrid/core_regrid.py
@@ -105,7 +105,7 @@ class InterpIndices:
 # throughout pipeline in regrid methods
 # add variable_config in _00_config?
 @dataclass
-class VectorData:
+class VectorField:
     u: npt.NDArray
     v: npt.NDArray
 
@@ -192,90 +192,43 @@ def compute_nearest_neighbor_indices(new_reg_grid: NewRegGrid, old_grid_proj: Ol
 
 
 def rotate_to_East_North(
-        old_grid_proj: OldGridProj
-):
+    u: npt.NDArray[npt.floating], v: npt.NDArray[npt.floating], 
+    old_grid_proj: OldGridProj, hemisphere: str
+    ) -> Tuple[npt.NDArray[npt.floating], npt.NDArray[npt.floating]]:
+    """
+    Rotates vector components to positive East/ North orientation
+    """
+
+    # Get rotation angle from longitude
+    theta = np.radians(old_grid_proj.lon_mesh)
+
+    # Rotate u and v vector components for Southern Hemisphere
+    if hemisphere.lower().strip() == 'sh':
+        u_rot = u * np.cos(theta) - v * np.sin(theta) 
+        v_rot = u * np.sin(theta) + v * np.cos(theta)
+
+    # Rotate u and v vector components for Northern Hemisphere
+    elif hemisphere.lower().strip() == 'nh':
+        u_rot = u * np.cos(theta) + v * np.sin(theta)
+        v_rot = -u * np.sin(theta) + v * np.cos(theta)
+
+    else:
+        # Handle case where hemisphere is not entered corectly
+        raise ValueError('"hemisphere" str must be "sh" (Southern) or "nh" (Northern)')
+
+    # FIXME dataclass to return vectorfield instead of Tuple to avoid mixup in returns?
+    return u_rot, v_rot
 
 
-def regrid_data(
-        *data: npt.NDArray[npt.floating], 
-        interp_indices: InterpIndices, 
-        old_grid_proj: OldGridProj,
-        new_reg_grid: NewRegGrid,
-        rotate_vectors: bool = False,
-):
-
-
-
-def nearest_neighbor_interpolation(
-    grid_resolution_deg: Tuple[float, float], latitude_bounds: Tuple[float, float], longitude_bounds: Tuple[float, float], 
-    old_latitude, old_longitude
-    ):
+def regrid_data(data: npt.NDArray[npt.floating], interp_indices: InterpIndices):
     """
     
     """
 
-    # Unpack resolution for latitude and longitude grid
-    latitude_resolution_deg, longitude_resolution_deg = grid_resolution_deg
+    # FIXME use list comprehension for all *data with 'yield' instead of 'return' for gracefulness?
 
-    # Unpack latitude tuple
-    min_latitude, max_latitude = latitude_bounds
+    # Handle case where data is not shaped [time, lat, lon]
+    if data.ndims != 3:
+        raise ValueError('data must be shaped [time, lat, lon]')
 
-    # Unpack longitude tuple
-    min_longitude, max_longitude = longitude_bounds
-
-    # Initialize new longitude coordinate array
-    new_latitude = np.arange(min_latitude, max_latitude + latitude_resolution_deg, latitude_resolution_deg)
-
-    # Initialize new latitude coordinate array
-    new_longitude = np.arange(min_longitude, max_longitude + longitude_resolution_deg, longitude_resolution_deg)
-
-    # Get number of new latitude longitude points
-    num_latitude = len(new_latitude)
-    num_longitude = len(new_longitude)
-
-    # Initialize interpolation indices
-    jj = np.empty((num_latitude, num_longitude))
-    ii = np.empty((num_latitude, num_longitude))
-
-    # Iterate through each new gridpoint
-    for j in range(num_latitude):
-        for i in range(num_longitude):
-
-            # Compute meridional distances
-            dy = (new_latitude[j] - old_latitude)**2
-
-            # Calculate zonal distances considering periodicity in longitude
-            dx1 = (new_longitude[i] - old_longitude) ** 2
-            dx2 = (new_longitude[i] - old_longitude + 360) ** 2
-            dx3 = (new_longitude[i] - old_longitude - 360) ** 2
-            
-            # Find the minimum zonal distance
-            dx = np.minimum(dx1, np.minimum(dx2, dx3))
-        
-            # Find minimum shortest distance
-            ds = dx + dy
-
-            # Find indices of minimum shortest distance
-            i_neighbors = np.where(ds == np.min(ds))
-
-            # Take minimum of the latitude and longitude indices (lower left corner)
-            jj[j,i] = np.min(i_neighbors[0])
-            ii[j,i] = np.min(i_neighbors[1])
-
-    # Return interpolation indices and new lat and lon coordinate variables
-    return jj, ii, new_latitude, new_longitude
-
-    
-
-def convert_km_to_degrees(grid_resolution_km, latitude_bounds, longitude_bounds):
-    """
-    
-    """
-
-    resolution_deg_latitude = grid_resolution_km / 111
-
-    average_latitude = np.nanmean(latitude_bounds)
-
-    resolution_deg_longitude = grid_resolution_km / 111 * np.cos(np.radians(average_latitude))
-
-    return (resolution_deg_latitude, resolution_deg_longitude)
+    return data[:,interp_indices.jj, interp_indices.ii]
