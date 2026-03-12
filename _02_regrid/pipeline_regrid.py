ce4c42b8b52bdc33c5c493ca17ac83fe0372279e
jackbassham <jbassham@ucsd.edu>
2026-03-12 10:06:08 -0700

    allow optional passing of vectors or scalars, return empty dict if None

--- _02_regrid/pipeline_regrid.py
+++ _02_regrid/pipeline_regrid.py
@@ -1,5 +1,5 @@
 import numpy.typing as npt
-from typing import Tuple, TYPE_CHECKING
+from typing import Tuple, Optional, TYPE_CHECKING
 
 from core_regrid import(
     OldGridProj,
@@ -12,11 +12,11 @@ from core_regrid import(
 
 
 def regrid_dataset(
-        scalar_fields: dict[str, npt.NDarrsy],
-        vector_fields: dict[str, Tuple[npt.NDarray, npt.NDarray]],
         old_grid_proj: OldGridProj,
         grid_specs: GridSpecs,
         hemisphere: str,
+        scalar_fields: Optional[dict[str, npt.NDarrsy]] = None,
+        vector_fields: Optional[dict[str, Tuple[npt.NDarray, npt.NDarray]]] = None,
         rotate_vectors: bool = False,
 ):
     
@@ -26,31 +26,42 @@ def regrid_dataset(
     # Compute nearest neighbor interpolation indices using new and old grids
     interp_indices = compute_nearest_neighbor_indices(new_reg_grid, old_grid_proj)
 
-
     # Initialize empty dict to store regrid vector field data names and tuples (u, v)
     vectors_regrid = {}
 
-    # Iterate through vector field tuples stored in dict
-    for name, (u, v) in vector_fields.items():
-        
-        # If vector fields call for rotation to postive East/North from x/y
-        if rotate_vectors:
-            # Rotate vector components
-            (u, v) = rotate_to_East_North(
-                u, v, old_grid_proj, hemisphere
-        )
+    if vector_fields is not None:
 
-        # Regrd each vector component in vector field and store in dict
-        vectors_regrid[name] = (
-            regrid_data(u, interp_indices),
-            regrid_data(v, interp_indices),
-        )
+        # Iterate through vector field tuples stored in dict
+        for name, (u, v) in vector_fields.items():
+            
+            # If vector fields call for rotation to postive East/North from x/y
+            if rotate_vectors:
+                # Rotate vector components
+                (u, v) = rotate_to_East_North(
+                    u, v, old_grid_proj, hemisphere
+            )
 
-    # Regrid scalar data and store names and data in dict
-    scalars_regrid = {
-        name: regrid_data(data, interp_indices)
-        for name, data in scalar_fields.items()
-    }
+            # Regrd each vector component in vector field and store in dict
+            vectors_regrid[name] = (
+                regrid_data(u, interp_indices),
+                regrid_data(v, interp_indices),
+            )
+
+    else: 
+        # If no vector entries, vectors_regrid remains emtpy
+        pass
+
+    # Initialize empty dict to store regrid scalar field data
+    scalars_regrid = {}
+
+    if scalar_fields is not None:
+        # Regrid scalar data and store names and data in dict
+        for name, data in scalar_fields.items():
+            scalar_fields[name] = (regrid_data(data, interp_indices))
+
+    else:
+        # If no scalar entries, scalars_regrid remains emtpy
+        pass
 
     return vectors_regrid, scalars_regrid, new_reg_grid
 
