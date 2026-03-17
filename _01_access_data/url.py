67edb229d96c997a5eb9b4f6634270de7adbd5b7
jackbassham <jbassham@ucsd.edu>
2026-03-16 17:07:18 -0700

    more notes on data locations

--- _01_access_data/url.py
+++ _01_access_data/url.py
@@ -25,9 +25,29 @@ class IceConcURL:
     DATA SET ID: NSIDC-0051
     DOI: 10.5067/MPYG15WAA4WX
     https://cmr.earthdata.nasa.gov/virtual-directory/collections/C3177837840-NSIDC_CPRD
+    NOTE It seems that Nimbus-7 sea ice concentrations only go until 2024.
+    2023 - Present Concentrations are in this dataset;
+    AMSR2 Daily Polar Gridded Sea Ice Concentrations, Version 2
+    https://nsidc.org/data/nsidc-0803/versions/2
+
+    Parent url:
+    https://daacdata.apps.nsidc.org/pub/DATASETS/nsidc0116_icemotion_vectors_v4/south/daily/
+    filename:
+    icemotion_daily_sh_25km_19900101_19901231_v4.1.nc
+
+
+    NOTE 
+    New root url:
+    https://cmr.earthdata.nasa.gov/virtual-directory/collections/C3177837840-NSIDC_CPRD/temporal/1989/01/01
+    New filename:
+    NSIDC0051_SEAICE_PS_S25km_19890101_v2.0.nc
     """
 
-    ...
+
+
+    parent_url: str
+    file_pattern: str
+    hemisphere_map: dict
 
 
 
