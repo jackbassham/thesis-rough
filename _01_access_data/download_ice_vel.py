36ddf12e82c4e2067609ddc8faa9ee5dc2892db0
jackbassham <jbassham@ucsd.edu>
2026-04-01 14:43:16 -0700

    implementing temp file from response content referencing nasa earth data script https://urs.earthdata.nasa.gov/documentation/for_users/data_access/python

--- _01_access_data/download_ice_vel.py
+++ _01_access_data/download_ice_vel.py
@@ -1,9 +1,13 @@
+import io
 import numpy as np
 from pathlib import Path
 import xarray as xr
 from typing import Tuple, Generator, TYPE_CHECKING
 if TYPE_CHECKING:
     from _00_config.config import PipelineConfig
+
+from earthdata_auth import create_earthdata_session
+from urls import IceVelURLBuilder
 
 def main(cfg: PipelineConfig):
 
@@ -12,14 +16,43 @@ def main(cfg: PipelineConfig):
     
     # Define raw data destination file name
     filename = 'ice_vel_raw_ppv4_ease.npz'
+    
+    # Create Nasa Earth Data session
+    earth_data_session = create_earthdata_session()
+
+    # Initialize url builder
+    url_builder = IceVelURLBuilder(cfg)
+
+    # Yield current URL from builder
+    url = url_builder.build()
+
+    # Get the temporary file from url
+    temp_file = get_temp_file(url, earth_data_session)
 
     ...
 
 
-
-
-
+def get_temp_file(url: str, session) -> io.BytesIO:
+    """
     
+    """
+
+    try:
+        # Submit a response request
+        response = session.get(url, stream = True)
+
+        # Print response status (200 for good response!)
+        print(response.status_code)
+
+    except:
+        # Handle http errors
+        response.raise_for_status()
+
+    # Read response content into temporary BytesIOobject
+    return io.BytesIO(response.content)
+
+
+
 if __name__ == "__main__":
     from _00_config.load_config import load_config
     cfg = load_config()
