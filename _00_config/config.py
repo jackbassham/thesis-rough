925e1be37d5ec8be3db8f22bab860b685f5590bf
jackbassham <jbassham@ucsd.edu>
2026-04-08 08:59:09 -0700

    change year range 1992-2020 in error handling

--- _00_config/config.py
+++ _00_config/config.py
@@ -88,7 +88,8 @@ class DataConfig:
             raise ValueError('Invalid year input: Enter years as integers in format YYYY')
 
         # Handle years out of range
-        if start < 1989 or end > 2024:
+        # NOTE for now, later 1992 - 2024 (sea ice conc) ok
+        if start < 1992 or end > 2020:
             raise ValueError('Years out of range: Enter in range 1989 to 2024')
 
         # Handle invalid order of range
