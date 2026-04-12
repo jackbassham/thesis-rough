bfb4e0f5739dbf18fd6cec37ba7e3abad6634667
jackbassham <jbassham@ucsd.edu>
2026-04-12 16:29:35 -0700

    fixed timestamp

--- run_era5.sh
+++ run_era5.sh
@@ -4,13 +4,13 @@ set -e # quit if error in step
 TS=04082026_0901
 
 echo "downloading era5..."
-python -m _01_access_data.download_wind --timestamp_out 04082026_0901 $TS
+python -m _01_access_data.download_wind --timestamp_out $TS
 echo "finished downloading era5"
 
 echo "regridding era5..."
-python -m _02_regrid.regrid_wind --timestamp_out 04082026_0901 $TS
+python -m _02_regrid.regrid_wind --timestamp_out $TS
 echo "finished regridding era5..."
 
 echo "running rest of pipeline with era5..."
-python -m run_pipeline --start mask_normalize --timestamp_out 04082026_0901 $TS
+python -m run_pipeline --start mask_normalize --timestamp_out $TS
 echo "finished pipeline"
