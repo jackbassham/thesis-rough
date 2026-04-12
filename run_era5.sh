9ffd36378da3ff5aee3866712b8c1687081c71ec
jackbassham <jbassham@ucsd.edu>
2026-04-12 16:26:41 -0700

    a shell script to test pipeline with era5

---
+++ run_era5.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e # quit if error in step
+
+TS=04082026_0901
+
+echo "downloading era5..."
+python -m _01_access_data.download_wind --timestamp_out 04082026_0901 $TS
+echo "finished downloading era5"
+
+echo "regridding era5..."
+python -m _02_regrid.regrid_wind --timestamp_out 04082026_0901 $TS
+echo "finished regridding era5..."
+
+echo "running rest of pipeline with era5..."
+python -m run_pipeline --start mask_normalize --timestamp_out 04082026_0901 $TS
+echo "finished pipeline"
