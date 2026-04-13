cd9d1f812fe55461f8687f44fa83cca1b1543017
jackbassham <jbassham@ucsd.edu>
2026-04-13 10:34:23 -0700

    update timestamps for shell run era5

--- run_era5.sh
+++ run_era5.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 set -e # quit if error in step
 
-TS=04082026_0901
+TS_RAW=04082026_0901
+
 
 echo "downloading era5..."
-python -m _01_access_data.download_wind --timestamp_out $TS
+python -m _01_access_data.download_wind --timestamp_raw $TS_RAW
 echo "finished downloading era5"
 
 echo "regridding era5..."
-python -m _02_regrid.regrid_wind --timestamp_out $TS
+python -m _02_regrid.regrid_wind --timestamp_raw $TS_RAW
 echo "finished regridding era5..."
 
 echo "running rest of pipeline with era5..."
-python -m run_pipeline --start mask_normalize --timestamp_out $TS
+python -m run_pipeline --start mask_normalize --timestamp_regrid $TS_RAW --timstampe_coordinates $TS_RAW
 echo "finished pipeline"
