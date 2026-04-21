f6af6d1124416d89a7572ce44d4f585dd7fb73ab
jackbassham <jbassham@ucsd.edu>
2026-04-21 14:38:26 -0700

    removing dict from make_complex_features_targets

--- _06_lr/lr_utils.py
+++ _06_lr/lr_utils.py
@@ -19,50 +19,62 @@ def load_input_data_from_npz(config):
     x_test, y_test, mask_test = test['x'], test['y'], test['mask']
 
 
-def make_complex_inputs(data: dict[str, npt.NDArray[np.float32]], include_uncertainty: bool=False):
+def make_complex_features_targets(x, y):
     """
     
     """
 
-    # For all arrays in data
-    for name, array in data.items():
-        # Convert nan values to 0
-        data[name] = np.nan_to_num(array, nan=0.0)
+    # Convert nans to zeros
+    x = np.nan_to_num(x, nan=0.0)
+    y = np.nan_to_num(y, nan=0.0)
 
     # Unpack features
-    ua_t0 = data['x'][:,0,:,:]
-    va_t0 = data['x'][:,1,:,:]
-    ci_t1 = data['x'][:,2,:,:]
+    ua_t0, va_t0, ci_t1 = x[:,0], x[:,1], x[:,2]
     # TODO include mask values as weights in LR
     # NOTE Going to experiment with including mask in both LR and CNN
-    mask = data['x'][:,3,:,:]
-
-    # Unpack targets
-    ui_t0 = data['y'][:,0,:,:]
-    vi_t0 = data['y'][:,1,:,:]
+    mask = x[:,3]
 
     # Make complex features
     za_t0 = ua_t0 + va_t0*1j # Complex pesent day day wind vector
     zci_t1 = ci_t1 + ci_t1*1j # Complex previous day ice concentration
 
+    # Unpack targets
+    ui_t0, vi_t0 = y[:,0], y[:,1]
+
     # Make complex targets
     zi_t0 = ui_t0 + vi_t0*1j # Complex previous day ice velocity vector
 
-    if include_uncertainty:
-        # Unpack uncertainty
-        ri_t0 = np.squeeze(data['ri_t0'], axis=1)
+    return za_t0, zci_t1, zi_t0
 
-        # Convert squared uncertainty to complex
-        # NOTE squared uncertainty used for weighting
-        # z_r**2 = (r_u + ir_v)(r_u - ir_v)
-        #      = r_u**2 + r_v**2
-        #   if r_u = r_v = r, z_r**2 = 2r**2  
-        zri_t0 = 2 * ri_t0 ** 2
 
-        return za_t0, zci_t1, zi_t0, zri_t0
+def get_input_dimensions(input: dict[str, npt.NDArray]):
+    """
     
-    else: 
-        return za_t0, zci_t1, zi_t0
+    """
+
+    # Get dimensions from first input assuming shape (time, channel, height, width)
+    in_channels, height, width = np.shape(next(iter(input.values())))[1:]
+
+    return(
+        in_channels,
+        height,
+        width
+    )
+
+
+def make_complex_weights(uncertainty):
+    """
+    
+    """
+
+    # Convert squared uncertainty to complex
+    # NOTE squared uncertainty used for weighting
+    # z_r**2 = (r_u + ir_v)(r_u - ir_v)
+    #      = r_u**2 + r_v**2
+    #   if r_u = r_v = r, z_r**2 = 2r**2  
+    uncertainty = 2 * uncertainty ** 2
+
+    return uncertainty
 
 
 def build_gram_and_data_matrices(targets: tuple, features: tuple, uncertainty: np.ndarray | None = None):
@@ -78,16 +90,5 @@ def build_gram_and_data_matrices(targets: tuple, features: tuple, uncertainty: n
     
     # Transpose targets into data matrix
     d = targets.T
-
-    if uncertainty is not None:
-        # NOTE uncertainty argument for weighting is complex, squared uncertainty
-        W = 1 / (uncertainty + 1e-4)
-
-        # NOTE, applying weights early on to avoid diags() memory useage
-        G_w = G * W[:, None]
-        d_w = d * W
-
-        return G_w, d_w
     
-    else:
-        return G, d
+    return (G, d)
