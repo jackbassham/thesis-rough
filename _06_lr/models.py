d2413b171991991e05b5c32ece033b169dc828c9
jackbassham <jbassham@ucsd.edu>
2026-04-21 14:40:50 -0700

    starting classes for lr and weighted lr gridwise

---
+++ _06_lr/models.py
@@ -0,0 +1,27 @@
+class GridwiseLR():
+    def __init__(self, in_channels, height, width):
+        # Get input dimensions
+        self.in_channels = in_channels
+        self.height = height
+        self.width = width
+
+
+
+    def fit(self, x, y):
+        """
+        
+        """
+
+
+    def test(self, x, parameters):
+        """
+        
+        """
+
+
+class GridwiseWeightedLR():
+    def __init__(self, in_channels, height, width):
+        # Get input dimensions
+        self.in_channels = in_channels
+        self.height = height
+        self.width = width
