18c8cf84b9b9e96062564b4283617accade2d43e
jackbassham <jbassham@ucsd.edu>
2026-04-24 12:03:42 -0700

    refactored baseline for config and model class

--- _05_train_models/train_baseline.py
+++ _05_train_models/train_baseline.py
@@ -1,44 +1,39 @@
 import numpy as np
 import os
+from . import models
 
-from _00_config.path import(
-    PATH_MODEL_INPUTS,
-    PATH_PS_OUT,
-)
+def main(cfg):
 
-def main():
+    # Load model inputs source path
+    path_model_inputs = cfg.path_config.data_stage_path('model_inputs')
 
-    # Load in data (same as lr inputs)
-    data = np.load(os.path.join(PATH_MODEL_INPUTS, f'test.npz'))
+    # Load in testing data for predictions
+    test = np.load(path_model_inputs / 'test.npz')
+    # Get targets from data, excluding mask (last feature)
+    y_test = test['y']
 
-    # Use test split for persistence baseline
-    y_test = data['y_test']
+    # Instantiate one day persistence baseline model
+    model = models.PersistenceBaseline()
 
-    # Get dimensions for output arrays
-    nt, n_out, nlat, nlon = np.shape(y_test)
+    # Get predictions on test splilt
+    y_pred = model.predict(y_test)
 
-    # Initialize output arrays
-    y_true = np.full((nt - 1, n_out, nlat, nlon), np.nan)
-    y_pred = y_true
+    # Load model output destination path
+    path_out = cfg.path_config.model_path('ps')
 
-    # Set predicted equal to previous day (remove last day from array)
-    y_pred = y_test[:-1,:,:,:]
-
-    # Shift true array forward
-    y_true = y_test[1:,:,:,:]
-
-    # Create the destination directory if it doesn't already exist
-    os.makedirs(PATH_PS_OUT, exist_ok = True)
+    # Make destination directory if missing
+    cfg.path_config.makedir_if_missing(path_out)
 
     # Save predictions
     np.savez(
-        os.path.join(PATH_PS_OUT, f"preds_ps.npz"),
+        path_out / 'preds.npz',
         y_pred = y_pred,
-        y_true = y_true
+        y_true = y_test,
     )
 
-    return
 
 if __name__ == "__main__":
-    main()
+    from _00_config.load_config import load_config
+    cfg = load_config()
+    main(cfg)
 
