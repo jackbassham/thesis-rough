3279abd9f1a45404dfce89b7b662172d62ee29d1
jackbassham <jbassham@ucsd.edu>
2026-04-24 18:23:40 -0700

    fixed bug: need to use full array of years for ie: np.isin(years, test_years) for selecting all the time indices where that year matches the test year

--- _04_process_inputs/split_generators.py
+++ _04_process_inputs/split_generators.py
@@ -11,8 +11,11 @@ def chronological_indices(
     
     """
 
-    # Get array of years from the time array
-    unique_years = np.unique(time.astype('datetime64[Y]'))
+    # Get array of n_time years from the time array
+    years = time.astype('datetime64[Y]')
+
+    # Get array of unique years for the splits
+    unique_years = np.unique(years)
 
     # Check that years will work for split
     validate_split_years(unique_years, n_val, n_test)
@@ -30,9 +33,9 @@ def chronological_indices(
     # Fill list with split indices dict
     split_indices = [
         {
-            'test': np.where(np.isin(unique_years, test_years))[0],
-            'val': np.where(np.isin(unique_years, val_years))[0],
-            'train': np.where(np.isin(unique_years, train_years))[0]  
+            'test': np.where(np.isin(years, test_years))[0],
+            'val': np.where(np.isin(years, val_years))[0],
+            'train': np.where(np.isin(years, train_years))[0]  
         }     
     ]
 
@@ -50,34 +53,39 @@ def k_randomized_year_indices(
     """
 
     # Get array of n_time years from the time array
-    unique_years = np.unique(time.astype('datetime64[Y]'))
+    years = time.astype('datetime64[Y]')
 
-    # Initialize empty list for ensemble member split indices
-    split_indices = []
+    # Get array of unique years for the splits
+    unique_years = np.unique(years)
 
     # Check that years will work for split
     validate_split_years(unique_years, n_val, n_test)
+
+    # Initialize empty list for ensemble member split indices
+    split_indices = []
 
     # Initialize random number generator with seed
     rng = np.random.default_rng(seed)
 
     for member in range(n_members):
+
+        # TODO label members
         
-        # Randomly permutate the array of years
+        # Randomly permutate the array of unique years
         shuffled_years = rng.permutation(unique_years)
 
         # The last 'n_test' years make test split
         test_years = shuffled_years[-n_test:]
         # The next 'n_val' years make the validation split
-        val_years = unique_years[-(n_test + n_val):-n_test]
+        val_years = shuffled_years[-(n_test + n_val):-n_test]
         # The remaining years in range make the training split
-        train_years = unique_years[:-(n_test + n_val)]
+        train_years = shuffled_years[:-(n_test + n_val)]
 
         # Append member's split indices to the list
         split_indices.append({
-            'test': np.where(np.isin(unique_years, test_years))[0],
-            'val': np.where(np.isin(unique_years, val_years))[0],
-            'train': np.where(np.isin(unique_years, train_years))[0]  
+            'test': np.where(np.isin(years, test_years))[0],
+            'val': np.where(np.isin(years, val_years))[0],
+            'train': np.where(np.isin(years, train_years))[0]  
         })   
 
     return split_indices
