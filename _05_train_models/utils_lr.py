ad91d1eec7ffb5027f4be253d30955bc24227959
jackbassham <jbassham@ucsd.edu>
2026-04-28 11:11:02 -0700

    remove function to load npz arrays

--- _05_train_models/utils_lr.py
+++ _05_train_models/utils_lr.py
@@ -2,23 +2,6 @@ import numpy as np
 import numpy.typing as npt
 
 
-def load_input_data_from_npz(config):
-    """
-    
-    """
-
-    # Load model inputs source path
-    path_model_inputs = config.path_config.data_stage_path('model_inputs')
-
-    # Return train and test npz data
-    train = np.load(path_model_inputs / 'train.npz')
-    test = np.load(path_model_inputs / 'test.npz')
-
-    # Get inputs from npz data
-    x_train, y_train, mask_train = train['x'], train['y'], train['mask']
-    x_test, y_test, mask_test = test['x'], test['y'], test['mask']
-
-
 def build_complex_features(x):
     """
     
