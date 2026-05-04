5703e362d9bf0934c495f494237a20dd1010a561
jackbassham <jbassham@ucsd.edu>
2026-05-04 14:01:16 -0700

    modified print statement to print mean of global var true

--- _06_evaluate/metric_fcns.py
+++ _06_evaluate/metric_fcns.py
@@ -63,7 +63,7 @@ def skill(pred, true, global_var_true=None, epsilon = 1e-4):
         vartrue = np.nanmean((true - truebar)**2, axis = 0) # variance in true
         # NOTE above is equivalent to np.nanvar()
 
-    print(f'Using VarTrue = {np.nanmean(vartrue)}')
+    print(f'Using VarTrue (NOTE print is mean over grid) {np.nanmean(vartrue)}')
 
     skill = 1 - mse / (vartrue + epsilon)
 
