f33be7d30f19adad1e29abbb23dea3cdd986844a
jackbassham <jbassham@ucsd.edu>
2026-05-06 13:44:31 -0700

    no global vartrue

--- analysis/skill_fcn_month.py
+++ analysis/skill_fcn_month.py
@@ -98,11 +98,11 @@ def main():
             preds = np.where(mask[:, None, :, :], np.nan, preds_list[m]) # (time, channel, height, width)
             trues = np.where(mask[:, None, :, :], np.nan, trues_list[m]) # (time, channel, height, width)
 
-            if 'skill' in metric_str:
-                global_true_bar = np.nanmean(trues, axis=0) # (channel, height, width)
-                global_var_true = np.nanmean((trues - global_true_bar)**2, axis=0) # (channel, height, width)
-            else:
-                global_var_true = None
+            # if 'skill' in metric_str:
+            #     global_true_bar = np.nanmean(trues, axis=0) # (channel, height, width)
+            #     global_var_true = np.nanmean((trues - global_true_bar)**2, axis=0) # (channel, height, width)
+            # else:
+            #     global_var_true = None
 
             monthly_metric = metric_fcn_month(
                 preds,
@@ -110,7 +110,7 @@ def main():
                 time[test_indices[f'{m:02d}']],
                 metric_fcn,
                 r=r,
-                global_var_true=global_var_true,
+                # global_var_true=global_var_true,
             )  # (month, channel, height, width)
 
             print(f'Finished member {m} of {N_MEMBERS} for {metric_str}')
