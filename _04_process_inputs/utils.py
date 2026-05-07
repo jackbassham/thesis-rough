7b768303c2039e892205a7eeb598121230f7f32b
jackbassham <jbassham@ucsd.edu>
2026-05-07 10:05:15 -0700

    reincluding mask in features array

--- _04_process_inputs/utils.py
+++ _04_process_inputs/utils.py
@@ -20,6 +20,7 @@ def make_target_feature_arrays(inputs: dict[str, npt.NDArray]
         'ua_t0',
         'va_t0',
         'ci_t1',
+        'mask',
     ]
 
     # Get input dimensions from first input
