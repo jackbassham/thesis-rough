0840fad281b11cd9da6308659722d161f9356fe0
jackbassham <jbassham@ucsd.edu>
2026-05-06 17:28:26 -0700

    created weighted nrmse for consistency with hoffman nrmse for unweighted cnn

--- _05_train_models/loss_funcs.py
+++ _05_train_models/loss_funcs.py
@@ -19,6 +19,21 @@ def weighted_mse(input, target, uncertainty, eps = 1e-6):
     # Return weighted mean square error
     return torch.sum(wse) / (torch.sum(w) + eps)
 
+def weighted_nrmse(input, target, uncertainty, eps = 1e-6):
+    # NOTE must think about w = 1 / (uncertainty**2 + eps) to match weighted linear regression 
+    # Weighted mse is used for the closed form solution!
+
+    # Compute weights
+    w = 1 / (uncertainty**2 + eps)
+    
+    # Compute weighted square error
+    wse = w * (input - target)**2
+
+    # Compute weighted mean square error
+    mse = torch.sum(wse) / (torch.sum(w) + eps)
+
+    # Return the normalized root mean square error
+    return torch.sqrt(mse) / (torch.std(target, unbiased = False) + eps)
 
 # TODO 
 # def weighted_nrmse():
