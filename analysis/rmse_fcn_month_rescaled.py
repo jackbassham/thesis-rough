135ea44b117ce69fe477bb21196ad6ff3dbbd45d
jackbassham <jbassham@ucsd.edu>
2026-05-12 12:39:05 -0700

    trying skill with rescaled values for metrics

--- analysis/rmse_fcn_month_rescaled.py
+++ analysis/rmse_fcn_month_rescaled.py
@@ -46,7 +46,7 @@ BASE_PATH = Path(
 def main():
 
     # Define list of metric strings
-    metric_strs = ['rmse', 'weighted_rmse', 'mae', 'mean_misfit']
+    metric_strs = ['skill', 'rmse', 'weighted_rmse', 'mae', 'mean_misfit']
 
 
     plot_path_base = Path('/home/jbassham/jack/thesis-rough/plots/quick-eval/')
