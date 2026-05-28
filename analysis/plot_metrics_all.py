0c2dd0b5392b21b1756cfb9fd228b8f36f761619
jackbassham <jbassham@ucsd.edu>
2026-05-28 08:30:22 -0700

    adapted function from plot_fcns

--- analysis/plot_metrics_all.py
+++ analysis/plot_metrics_all.py
@@ -1,7 +1,7 @@
-import calendar
+import cartopy.crs as ccrs
+import cmocean as cmo
 import matplotlib.pyplot as plt
-from matplotlib.lines import Line2D
-from matplotlib.ticker import MaxNLocator
+import matplotlib.colors as mcolors
 import numpy as np
 from pathlib import Path
 
@@ -42,5 +42,95 @@ def main():
     # Make base plot directory
     PLOT_PATH.mkdir(parents=True, exist_ok=True)
 
-    # Month labels for titles
-    month_labels = [calendar.month_abbr[i+1] for i in range(12)]
+
+def plot_ensemble_all_models(
+        metrics,
+        lon,
+        lat,
+        hemisphere,
+        save_path,
+        figsize=(10,10),
+        cmap=cmo.cm.balance_r,
+        boundaries=np.arange(-0.8, 0.6+0.1, 0.1)
+    ):
+
+    #~~~~~~~~~~~~~~~~~~~~~
+    # Cartopy set up
+    #~~~~~~~~~~~~~~~~~~~~~
+    # Set longitude bounds for plot (full zonal coverage)
+    lon_min = -180
+    lon_max = 180
+
+    # Set latitude bounds and projection based on hemisphere
+    if hemisphere.lower().strip() == 'south':
+        lat_min = -90
+        lat_max = -65
+        projection = ccrs.SouthPolarStereo()
+    elif hemisphere.lower().strip() == 'north':
+        lat_min = 65
+        lat_max = 90
+        projection = ccrs.NorthPolarStereo()
+
+    else:
+        raise ValueError('Enter hemisphere argument as string "south" or "north"')
+    
+    # Define data-to-plot's coordinate reference system
+    # NOTE, used for 'crs' and 'transform' cartopy parameters
+    data_crs = ccrs.PlateCarree()
+    data_extent = [lon_min, lon_max, lat_min, lat_max]
+
+    fig, axs = plt.subplots(
+        3,
+        2,
+        figsize=figsize,
+        subplot_kw={"projection": projection},
+        constrained_layout=True,
+    )
+
+    #~~~~~~~~~~~~~~~~~~~~~
+    # Panel set up
+    #~~~~~~~~~~~~~~~~~~~~~
+    panels = [
+        ('cnn_pt_wtd', 'weighted_skill', 0, axs[0,0], r'(a) $u_{i,t}$')
+        ('cnn_pt_wtd', 'weighted_skill', 1, axs[0,1], r'(a) $v_{i,t}$')
+        ('lr_cf_wtd', 'weighted_skill', 0, axs[1,0], r'(b) $u_{i,t}$')
+        ('lr_cf_wtd', 'weighted_skill', 1, axs[1,1], r'(b) $v_{i,t}$')
+        ('ps', 'weighted_skill', 0, axs[2,0], r'(c) $u_{i,t}$')
+        ('ps', 'weighted_skill', 1, axs[2,1], r'(c) $v_{i,t}$')
+    ]
+
+    for model_str, metric_str, channel, ax, title in panels:
+
+        mean = metrics[model_str][metric_str]['mean'][channel]
+
+        ax.set_extent(data_extent, crs=data_crs)
+        ax.coastlines()
+
+        norm = mcolors.BoundaryNorm(boundaries, cmap.N)
+
+        pcm = ax.pcolormesh(
+            lon,
+            lat,
+            mean,
+            transform=data_crs,
+            cmap=cmo.cm.balanc_r,
+            norm=norm,
+        )
+
+        ax.set_title(title, fontweight='bold')
+
+    fig.colorbar(pcm, ax=axs, orientation='vertical', shrink=0.8, label=r'$Skill$')
+
+    plt.tight_layout(
+        rect=[0, 0, 0.86, 0.96]
+    )
+
+    # vector graphics for publications
+    plt.savefig(
+        save_path,
+        dpi=300,
+        bbox_inches="tight",
+    )
+
+    plt.close()
+
